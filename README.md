70f33360d5acfef89c03eb84e32933a516f623ab
戴兜 <daidr@daidr.me>
2020-01-21 09:49:59 +0800

    提交代码

---
+++ README.md
@@ -0,0 +1,46 @@
+<div align="center">
+  <h1>
+    明日方舟立绘反混淆
+  </h1>
+
+  <p>
+    <strong>使用nodejs的sharp库反混淆明日方舟立绘</strong>
+  </p>
+</div>
+
+## 直接下载
+
+提供已经反混淆的全套立绘图片，前往[release](https://github.com/daidr/ark_decoder/releases "Release")下载（可以忽略下面步骤）
+
+## 环境要求
+
+1. nodejs运行环境
+2. 磁盘剩余容量 > 1.5G
+
+## 开始使用
+
+1. 克隆仓库 `git clone https://github.com/daidr/ark_decoder.git`
+2. 执行 `yarn install`（推荐） 或 `npm i` （取决于你所使用的包管理器）
+3. 执行 `yarn all`（推荐） 或 `npm run all` （取决于你所使用的包管理器）
+
+## 分步执行
+
+> 注意：在执行decode步骤前，需要保证Backup步骤至少被执行过一次。
+
+### 使用 yarn 作为包管理器时
+
+- `yarn all` 执行下面的全部步骤
+- `yarn backup` 用于将原始立绘迁移至Texture_After目录下
+- `yarn decode` 用于将Texture_After目录下的立绘反混淆，并保存到Texture_Final目录
+
+### 使用 npm 作为包管理器时
+
+- `npm run all` 执行下面的全部步骤
+- `npm run backup` 用于将原始立绘迁移至Texture_After目录下
+- `npm run decode` 用于将Texture_After目录下的立绘反混淆，并保存到Texture_Final目录
+
+-----------
+
+[戴兜的小屋](http://im.daidr.me)
+
+最后更新时间： 2020.01.21
---
+++ main.js
@@ -0,0 +1,106 @@
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
+let couldDecode = false;
+
+var options = process.argv;
+for (var i = 0; i < 4; i++) {
+    switch (options[i]) {
+        case "-all":
+            couldDecode = true;
+            backupImage();
+            return;
+        case "-backup":
+            backupImage();
+            return;
+        case "-decode":
+            decodeImage();
+            return;
+        default:
+            break;
+    }
+}
+
+
+function backupImage() {
+    deleteDirectory('./Texture_After')
+    fs.mkdirSync("./Texture_After", { recursive: true });
+    fs.readdir("./Texture_Origin", function (err, files) {
+        console.log(`创建立绘备份，可能花费较长时间，请稍后...`);
+        let bt = new Date().getTime();
+        for (let i = 0; i < files.length; i++) {
+            fs.copyFileSync(`./Texture_Origin/${files[i]}`, `./Texture_After/${/(\w*#?[\d\w+]{0,7}(?:\[alpha\])?)\s{0,2}#?\S{0,8}\.png/.exec(files[i])[1]}.png`);
+        }
+        console.log(`立绘备份创建完成，用时${new Date().getTime() - bt}ms`);
+        if (couldDecode) {
+            decodeImage();
+        }
+    });
+}
+
+function decodeImage() {
+    deleteDirectory('./Texture_Final')
+    fs.mkdirSync("./Texture_Final", { recursive: true });
+    fs.readdir("./Texture_After", function (err, files) {
+        console.log(`开始反混淆立绘，请稍后...`);
+        bt = new Date().getTime();
+        goNext(0);
+        function goNext(i) {
+            if (!files[i].includes("[alpha]")) {
+                console.log(`${i}/${files.length} · 正在处理：${`./Texture_After/${files[i]}`}`);
+                (async function () {
+                    let mask = await sharp(`./Texture_After/${files[i].slice(0, -4) + "[alpha].png"}`)
+                        .extractChannel("red");
+
+                    let image = await sharp(`./Texture_After/${files[i]}`)
+                        .removeAlpha();
+
+                    let maskMetadata = await mask.metadata();
+                    let imageMetadata = await image.metadata();
+                    if (maskMetadata.width > imageMetadata.width) {
+                        await image.resize(maskMetadata.width, maskMetadata.height);
+                    } else if (maskMetadata.width < imageMetadata.width) {
+                        await mask.resize(imageMetadata.width, imageMetadata.height);
+                    }
+                    let maskBuffer = await mask.toBuffer();
+                    let imageBuffer = await image.toBuffer();
+                    sharp(imageBuffer)
+                        .joinChannel(maskBuffer)
+                        .toFile(`./Texture_Final/${files[i]}`, function (_err, _info) {
+                            if (i < files.length - 1) {
+                                goNext(i + 1);
+                            } else {
+                                console.log(`立绘反混淆完成，用时${new Date().getTime() - bt}ms`);
+                                console.log(`输出目录：./Texture_Final`);
+                            }
+                        })
+                })()
+            } else {
+                if (i < files.length - 1) {
+                    goNext(i + 1);
+                } else {
+                    console.log(`立绘反混淆完成，用时${new Date().getTime() - bt}ms`);
+                    console.log(`输出目录：./Texture_Final`);
+                }
+            }
+        }
+
+    });
+}
+
+function deleteDirectory(dir) {
+    if (fs.existsSync(dir) == true) {
+        var files = fs.readdirSync(dir);
+        files.forEach(function (item) {
+            var item_path = path.join(dir, item);
+            if (fs.statSync(item_path).isDirectory()) {
+                deleteDirectory(item_path);
+            }
+            else {
+                fs.unlinkSync(item_path);
+            }
+        });
+        fs.rmdirSync(dir);
+    }
+}
